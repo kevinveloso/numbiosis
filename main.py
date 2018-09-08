5780b4d9802e97303b9932a90857a00f68e49e35
valfredo-santos <valfredojr3@gmail.com>
2018-09-08 15:07:34 -0300

    Atualização do exemplo de execução

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import Metodos
 if __name__ == '__main__':
 
     #Exemplo de execucao:
-    test = Metodos('(x*x) + x - 6')
+    test = Metodos('x^2 + x - 6')
     test.falsaposicao(1.5, 2.5, 0.005, 10)
     test.secante(1.5, 2.5, 0.005, 10)
-    test.setFuncao('(x*x*x) - (x * 13) - 12')
+    test.setFuncao('x^3 - x * 13 - 12')
     test.muller(1.5, 3.0, 5.5, 0.05, 10)
